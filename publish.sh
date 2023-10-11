4f76c438d437e13a7f174a7aa701c79362e6afbe
Frits Hoogland <frits.hoogland@gmail.com>
2023-10-11 10:20:57 +0200

    correct worktree/branch

--- publish.sh
+++ publish.sh
@@ -1,20 +1,20 @@
 #
 # This is copied from https://github.com/rust-lang/mdBook/wiki/Automated-Deployment%3A-GitHub-Pages
 #
-git worktree add gh-page
+git worktree add gh-pages
 #
 mdbook build
 #
-rm -rf ./gh-page/* # this won't delete the .git directory
+rm -rf ./gh-pages/* # this won't delete the .git directory
 #
-cp -rp book/* ./gh-page/
+cp -rp book/* ./gh-pages/
 #
-cd ./gh-page
+cd ./gh-pages
 #
 git add -A
 #
 git commit -m 'deploy new book'
 
-git push origin gh-page
+git push origin gh-pages
 
 cd -
